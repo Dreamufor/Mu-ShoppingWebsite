760706f8fb92b696200a4f85deff26b71f6c912a
mu3122 <azimeng520@gmail.com>
2018-08-23 19:20:25 +1200

    master

---
+++ QualitySouvenir/QualitySouvenir/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QualitySouvenir.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
